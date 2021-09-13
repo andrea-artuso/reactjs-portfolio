c8c32716df786608b9ef0bda984699dc95812828
andrea-artuso <ultrandr3x@gmail.com>
2021-09-13 16:47:52 +0200

    Created projects page and added react routering

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,11 +1,46 @@
+import { useState, useEffect } from 'react'
+
+//Import STYLES
 import './Projects.css'
 
-const Projects = () => {
+const Header = () => {
+    const [searchFilter, setSearchFilter] = useState('');
+    const [projects, setProjects] = useState([]);
+    const [filteredProjects, setFilteredProjects] = useState([]);
+
+    useEffect(() => {
+        getProjectsData(setProjects);
+    }, [])
+
+    useEffect(() => {
+        setFilteredProjects(projects.filter(project => project.title.toLowerCase().includes(searchFilter)));
+    }, [projects, searchFilter])
+
     return (
-        <div>
-            <h1>Ciao a tutti</h1>
-        </div>
+        <>
+            <header className="">
+                <a href="/" >Return to homepage</a>
+
+                <input
+                    type="text"
+                    value={searchFilter}
+                    onChange={(e) => setSearchFilter(e.target.value)}
+                    placeholder="Search for projects"
+                />
+            </header>
+            <div>
+                {
+                    filteredProjects.map(project => <h1 key={project.id} >{project.title}</h1>)
+                }
+            </div>
+        </>
     )
 }
 
-export default Projects
+function getProjectsData(setProjects){
+    fetch('../../../data/Projects.json')
+      .then(response => response.json())
+      .then(data => setProjects(data))
+}  
+
+export default Header

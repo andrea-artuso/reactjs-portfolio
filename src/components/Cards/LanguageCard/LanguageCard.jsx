7983ec188afb5b33b763d686111a602d6e15f683
Andrea Artuso <ultrandr3x@gmail.com>
2021-10-25 18:56:48 +0200

    Fixed flags images

--- src/components/Cards/LanguageCard/LanguageCard.jsx
+++ src/components/Cards/LanguageCard/LanguageCard.jsx
@@ -4,7 +4,7 @@ import './LanguageCard.css'
 const LanguageCard = ( {name, level, code} ) => {
     return (
         <div className="card language-card">
-            <img src={`https://www.countryflags.io/${code.toLowerCase()}/flat/64.png`} alt={name}></img>
+            <img src={`https://www.countryflagicons.com/FLAT/64/${code.toUpperCase()}.png`} alt={name}></img>
             <div>
                 <h2>{name}</h2>
                 <p>{level}</p>
